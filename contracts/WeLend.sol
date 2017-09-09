3aca20eb8f740b189c376d92cb184c7b9bb7fc5d
Akshit Ahluwalia <akshitahluwalia@gmail.com>
2017-09-09 17:57:19 +0530

    mappings created

--- contracts/WeLend.sol
+++ contracts/WeLend.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.4.8;
 import "./owned.sol";
 import "./mortal.sol";
+
 contract WeLend {
-
-
+    Loan[] loans;
+    mapping(address => Loan[]) userLoans;
+    mapping()
     struct Loan {
         uint id;
+        address creator;
         address[] guarantors;
         uint community;
         string description;
