ccd4384e911c39a9307e107051e3fe87a98f2ae0
Akshit Ahluwalia <akshitahluwalia@gmail.com>
2017-09-09 17:49:38 +0530

    contract setup

--- contracts/WeLend.sol
+++ contracts/WeLend.sol
@@ -1,7 +1,23 @@
 pragma solidity ^0.4.8;
-
+import "./owned.sol";
+import "./mortal.sol";
 contract WeLend {
 
+
+    struct Loan {
+        uint id;
+        address[] guarantors;
+        uint community;
+        string description;
+        uint dueBy;//Months
+        uint amount;
+        bool bankApproved;
+        bool communityApproved;
+        bool funded;
+        bool canceled;
+        bool repayed;
+        uint interestRate;
+    }
     function WeLend(){
 
     }
